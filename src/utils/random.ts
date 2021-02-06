825469bee8cb5f5ed03a5742f223612f6ea5f08c
Quipex <rudas72@gmail.com>
2021-02-06 18:57:38 +0200

    Format

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -9,6 +9,5 @@ export function randomEnum<T>(anEnum: T): T[keyof T] {
     .map(n => Number.parseInt(n))
     .filter(n => !Number.isNaN(n)) as unknown as T[keyof T][]
   const randomIndex = Math.floor(Math.random() * enumValues.length)
-  const randomEnumValue = enumValues[randomIndex]
-  return randomEnumValue;
+  return enumValues[randomIndex];
 }
